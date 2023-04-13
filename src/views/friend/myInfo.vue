06a73f4acd582b20c81142b9ab293869273917c6
tongcheng <983123596@qq.com>
2023-04-13 18:31:11 +0800

    '3'

--- src/views/friend/myInfo.vue
+++ src/views/friend/myInfo.vue
@@ -102,17 +102,16 @@
                 this.imageUrl = URL.createObjectURL(file.raw);
             },
             beforeAvatarUpload(file) {
-                // const isJPG = file.type === 'image/jpeg';
-                // const isLt2M = file.size / 1024 / 1024 < 2;
-                //
-                // if (!isJPG) {
-                //     this.$message.error('上传头像图片只能是 JPG 格式!');
-                // }
-                // if (!isLt2M) {
-                //     this.$message.error('上传头像图片大小不能超过 2MB!');
-                // }
-                // return isJPG && isLt2M;
-                return true
+                const isJPG = file.type === 'image/jpeg';
+                const isLt2M = file.size / 1024 / 1024 < 2;
+
+                if (!isJPG) {
+                    this.$message.error('上传头像图片只能是 JPG 格式!');
+                }
+                if (!isLt2M) {
+                    this.$message.error('上传头像图片大小不能超过 2MB!');
+                }
+                return isJPG && isLt2M;
             },
 
             async toNextPage(to) {
